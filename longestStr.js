e4c147d8c42ab5fac1e3a4c3e1c61d2c2c0a761b
Eduard Harutyunyan <itm.eduard.harutyunyan@gmail.com>
2021-12-05 23:09:01 +0400

    feat: added longestStr.js file but did tasks not clearly

---
+++ longestStr.js
@@ -0,0 +1,16 @@
+// "use strict"
+
+// function longestStr(sentence){
+//     let splitEd = sentence.split(" ")
+//     let needed = '';
+//     for(let i = 0; i < splitEd.length; i++){
+//         if(splitEd.length > needed.length){
+//             needed = splitEd[i];
+//         }
+//     }
+//     return needed;
+// }
+// console.log(longestStr("A revolution without dancing is a revolution not worth having."))
+
+
+
