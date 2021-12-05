ebe4dc3514c68e37e373ee237f6dc210c34a7ecd
Eduard Harutyunyan <itm.eduard.harutyunyan@gmail.com>
2021-12-05 23:11:14 +0400

    feat: added oddEven.js file and did tasks

---
+++ oddEvenFunc.js
@@ -0,0 +1,18 @@
+// "use strict"
+
+// function oddEven(arr){
+//     let odd = [];
+//     let even = [];
+//     let concatEd = []
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i] % 2 === 0){
+//             even.push(arr[i])
+//         }
+//         else if(arr[i] % 2 === 1){
+//             odd.push(arr[i])
+//         }
+//     }
+//      concatEd.push(even,odd);
+//     return concatEd;
+// }
+// console.log(oddEven([1,3,5,9]))
